e4c181d8f42613aa41f133b8082526531f13acf1
Edison Chancusig <47606288+ediferchancusig@users.noreply.github.com>
2020-09-01 22:21:50 -0500

    Docs agregado

---
+++ docs/precache-manifest.f3710b77c9e8889e646a89d94d613412.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8abe8dc96f9fbb40676c3e30e0012a5e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d710e5f168b7edfab977",
+    "url": "/static/css/main.33aa2db3.chunk.css"
+  },
+  {
+    "revision": "85a4a86f3fe06f17fcc7",
+    "url": "/static/js/2.8249eaeb.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.8249eaeb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d710e5f168b7edfab977",
+    "url": "/static/js/main.14fda5ee.chunk.js"
+  },
+  {
+    "revision": "daee87f482498cdf724c",
+    "url": "/static/js/runtime-main.29242b1d.js"
+  }
+]);
